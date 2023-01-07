3123cf75b4bc4ed985097ac9794f507e4d92aa7a
Dmitrij Manin <manin.dmitrij@gmail.com>
2023-01-07 22:12:06 +0300

    created test function to authorization logic

--- lessons/lesson_3_6/step_5.py
+++ lessons/lesson_3_6/step_5.py
@@ -1,10 +1,25 @@
-import pytest
 from selenium.webdriver.common.by import By
-from decouple import config
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+
+import time
 
 
-@pytest.mark.parametrize('language', config('language', cast=lambda lang: [el.strip() for el in lang.split(',')]))
-def test_guest_should_see_login_link(browser, language):
-    link = f"http://selenium1py.pythonanywhere.com/{language}/"
-    browser.get(link)
-    browser.find_element(By.CSS_SELECTOR, "#login_link")
+# Test authorization logic
+def test_alien_message(browser):
+    browser.get('https://stepik.org/lesson/236895/step/1')
+    try:
+        login_button = WebDriverWait(browser, 10).until(ec.presence_of_element_located((By.ID, 'ember33')))
+        login_button.click()
+        
+        email_input = browser.find_element(By.ID, 'id_login_email')
+        email_input.send_keys('test_login')
+
+        psw_input = browser.find_element(By.ID, 'id_login_password')
+        psw_input.send_keys('test_password')
+
+        accept_button = browser.find_element(By.CLASS_NAME, 'sign-form__btn')
+        accept_button.click()
+        time.sleep(3)
+    except Exception as e:
+        print('some error happened', e)
