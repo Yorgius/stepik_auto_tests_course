31dde2e9b51a98ac93bb4abe7d3f2187850d89f9
Dmitrij Manin <manin.dmitrij@gmail.com>
2023-01-14 09:14:45 +0300

    corrected func name

--- lessons/lesson_3_6_to_recense/test_items.py
+++ lessons/lesson_3_6_to_recense/test_items.py
@@ -4,7 +4,7 @@ import time
 
 
 # test for the presence of the button <add to cart> on the page with different language settings
-def test_search_add_to_cart_button(browser):
+def test_check_for_add_to_cart_button(browser):
     url = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     browser.get(url)
 
